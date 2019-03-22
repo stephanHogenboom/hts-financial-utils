55e680a46d5631f6903563fcf0e87f2b184cbc09
Stephan Hogenboom <stephan.hogenboom@dans.knaw.nl>
2019-03-22 11:42:30 +0100

    added data retrieval and gitignroe

--- src/main/java/com/hogenboom/finane/utils/gui/MainScreen.java
+++ src/main/java/com/hogenboom/finane/utils/gui/MainScreen.java
@@ -20,74 +20,83 @@ import java.nio.file.Paths;
 
 public class MainScreen extends Application {
 
-    private TabPane rootLayout = new TabPane();
-    private FinancialRecordManager manager = null;
-    private Tab dataTab = new Tab("DataGrid");
-    private Label managerLabel = new Label("Empty");
-    private Tab barTab = new Tab("Bar Charts");
-    private Tab monthDetailTab = new Tab("Month Details");
-    private Path dataPath = Paths.get("data");
+  private TabPane rootLayout = new TabPane();
+  private FinancialRecordManager manager = null;
+  private Tab dataTab = new Tab("DataGrid");
+  private Label managerLabel = new Label("Empty");
+  private Tab barTab = new Tab("Bar Charts");
+  private Tab monthDetailTab = new Tab("Month Details");
+  private Path dataPath = Paths.get("data");
+  private Path dataFile = dataPath.resolve("data.csv");
 
-    public static void main(String[] args) {
-        launch(args);
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Button importButton = new Button("import");
+    Button saveButton = new Button("save");
+
+    FileChooser chooser = new FileChooser();
+    importButton.setOnAction(e -> {
+      File file = chooser.showOpenDialog(primaryStage);
+      if (file != null) {
+        openFile(file, primaryStage);
+      }
+    });
+
+    saveButton.setOnAction(event -> {
+      saveFile();
+      managerLabel.setText("saved");
+    });
+
+    FlowPane importScreen = new FlowPane();
+    Tab importTab = new Tab("Import CSV");
+
+    rootLayout.getTabs().addAll(importTab, dataTab, barTab, monthDetailTab);
+    importTab.setContent(importScreen);
+    importScreen.getChildren().addAll(importButton, saveButton, managerLabel);
+    Scene scene = new Scene(rootLayout, 800, 800);
+    primaryStage.setScene(scene);
+    primaryStage.setTitle("Finance tools");
+    primaryStage.show();
+    if (Files.exists(dataFile)){
+      openFile(dataFile.toFile(), primaryStage);
+    } else {
+      managerLabel.setText("No datafile found");
     }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Button importButton = new Button("import");
-        Button saveButton = new Button("save");
+  }
 
-        FileChooser chooser = new FileChooser();
-        importButton.setOnAction(e -> {
-            File file = chooser.showOpenDialog(primaryStage);
-            if (file != null) {
-                openFile(file, primaryStage);
-            }
-        });
-
-        saveButton.setOnAction( event -> {
-            saveFile();
-            managerLabel.setText("saved");
-        });
-
-        FlowPane importScreen = new FlowPane();
-        Tab importTab = new Tab("Import CSV");
-
-        rootLayout.getTabs().addAll(importTab, dataTab, barTab, monthDetailTab);
-        importTab.setContent(importScreen);
-        importScreen.getChildren().addAll(importButton, saveButton, managerLabel);
-        Scene scene = new Scene(rootLayout, 800, 800);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Finance tools");
-        primaryStage.show();
+  private void openFile(File file, Stage primaryStage) {
+    try {
+      manager = FinancialRecordManager.fromString(file.toString(), true);
+      managerLabel.setText(file.getName());
+      dataTab.setContent(DataGrid.createTableView(manager));
+      barTab.setContent(TotalPerMonthBarChart.createTotalPerMonth(manager));
+      monthDetailTab.setContent(MonthlyExpenditureGridView.createMonthlyExpenditureGridView(manager, primaryStage));
     }
-
-    private void openFile(File file, Stage primaryStage) {
-        try {
-            manager = FinancialRecordManager.fromString(file.toString(), true);
-            managerLabel.setText(file.getName());
-            dataTab.setContent(DataGrid.createTableView(manager));
-            barTab.setContent(TotalPerMonthBarChart.createTotalPerMonth(manager));
-            monthDetailTab.setContent(MonthlyExpenditureGridView.createMonthlyExpenditureGridView(manager, primaryStage));
-        } catch (Exception ex) {
-            System.out.println("exception occured:" + ex.getMessage());
-            ex.printStackTrace();
-        }
+    catch (Exception ex) {
+      System.out.println("exception occured:" + ex.getMessage());
+      ex.printStackTrace();
     }
+  }
 
-    private void saveFile() {
-        try {
-            if (manager == null) {
-                throw new IllegalStateException("Manager is not initialized");
-            }
-            if (!Files.exists(dataPath)) {
-                Files.createDirectory(dataPath);
-            }
+  private void saveFile() {
+    try {
+      if (manager == null) {
+        throw new IllegalStateException("Manager is not initialized");
+      }
+      if (!Files.exists(dataPath)) {
+        Files.createDirectory(dataPath);
+      }
 
-            CSVExporter exporter = new CSVExporter();
-            exporter.writeData(manager.getRecords(), dataPath.resolve("data.csv").toFile());
-        } catch (Exception e) {
-            managerLabel.setText("Error: " + e.getMessage());
-        }
+      CSVExporter exporter = new CSVExporter();
+      exporter.writeData(manager.getRecords(), dataFile.toFile());
     }
+    catch (Exception e) {
+      managerLabel.setText("Error: " + e.getMessage());
+    }
+  }
 }
